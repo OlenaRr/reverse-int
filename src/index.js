0ec07727c6198f7a12439748b9c31c905fe6603d
OlenaRr <olenkar093@gmail.com>
2022-01-17 22:54:16 +0200

    feat: added function logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (numb) {
+    answ = ""
+	if (typeof (numb) === "number") {
+		for (let i = (Math.abs(numb).toString()).length - 1; i >= 0 ; i--) {
+			answ += Math.abs(numb).toString()[i];
+		}
+		return Number(answ);
+	}
+	else {
+		answ = "Not a number!";
+		return answ;
+	}
 }
